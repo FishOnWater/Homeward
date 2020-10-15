0eab1cc4d841daf9c8385e4494a143f3ce40a509
Thewildcard-pt <thewildcardpt@gmail.com>
2020-10-15 18:23:26 +0100

    Added Super basic movement and jump
    Can jump and check ground.
    Movement works but has no clamp so the player can accelerate to infinity.

---
+++ Assets/Scripts/Scr_mov.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_mov : MonoBehaviour
+{
+    public float movespeed = 0.025f;
+    public float JumpForce = 10f;
+    public bool isGrounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.time * movespeed;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+    }
+}
