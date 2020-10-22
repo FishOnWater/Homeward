797033ca5e2a38855f0d1e723208b270c146784b
Fish On Water <luismiguelfigueiras@gmail.com>
2020-10-22 18:28:33 +0100

    Grappling Hook implemented

--- Assets/Scripts/Scr_mov.cs
+++ Assets/Scripts/Scr_mov.cs
@@ -7,6 +7,11 @@ public class Scr_mov : MonoBehaviour
     public float movespeed = 0.025f;
     public float JumpForce = 10f;
     public bool isGrounded = false;
+
+    public Camera cam;
+    public GameObject crosshairs;
+
+    Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,12 @@ public class Scr_mov : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.Translate(movement * (Time.deltaTime * movespeed),Space.World);
 
+        //é preciso isto para fazer o cálculo da posição real do rato no mundo
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //cálculo da direção do rato (sim, era mesmo só isto)
+        Vector2 lookDir = mousePos - gameObject.GetComponent<Rigidbody2D>().position;
+        crosshairs.transform.position = new Vector2(mousePos.x, mousePos.y);
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             Jump();
@@ -27,7 +38,7 @@ public class Scr_mov : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             //ShootHook(Input.mousePosition.x, Input.mousePosition.y);
-            ShootHook(Input.mousePosition);
+            ShootHook(lookDir);
         }
     }
 
@@ -37,12 +48,12 @@ public class Scr_mov : MonoBehaviour
     }
 
     //void ShootHook(float xpos, float ypos)
-    void ShootHook(Vector3 mPos)
+    void ShootHook(Vector2 mDir)
     {
-        Vector3 diff = mPos - gameObject.transform.position;
-        float distance = diff.magnitude;
-        Vector2 mDir = diff / distance;
-        mDir.Normalize();  
+        //Vector3 diff = mPos - gameObject.transform.position;
+        //float distance = diff.magnitude;
+        //Vector2 mDir = diff / distance;
+        //mDir.Normalize();  
 
         //this part is fucked
         RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, mDir, 5000f, 1<<LayerMask.NameToLayer("Action"));
