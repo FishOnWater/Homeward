c8252940419d9bdc372c2d93eebc9fdc093468d7
Fish On Water <luismiguelfigueiras@gmail.com>
2020-10-22 17:09:33 +0100

    Some raycast implementation

---
+++ Assets/Scripts/PointHook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointHook : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject crosshairs;
+    private Vector3 mouseTarget;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseTarget = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshairs.transform.position = new Vector2(mouseTarget.x, mouseTarget.y);   
+    }
+}
--- Assets/Scripts/Scr_mov.cs
+++ Assets/Scripts/Scr_mov.cs
@@ -19,8 +19,6 @@ public class Scr_mov : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.Translate(movement * (Time.deltaTime * movespeed),Space.World);
 
-
-
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             Jump();
@@ -28,8 +26,8 @@ public class Scr_mov : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            ShootHook(Input.mousePosition.x, Input.mousePosition.y);
-            
+            //ShootHook(Input.mousePosition.x, Input.mousePosition.y);
+            ShootHook(Input.mousePosition);
         }
     }
 
@@ -38,17 +36,26 @@ public class Scr_mov : MonoBehaviour
         gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
     }
 
-    void ShootHook(float xpos, float ypos)
+    //void ShootHook(float xpos, float ypos)
+    void ShootHook(Vector3 mPos)
     {
+        Vector3 diff = mPos - gameObject.transform.position;
+        float distance = diff.magnitude;
+        Vector2 mDir = diff / distance;
+        mDir.Normalize();  
+
         //this part is fucked
-        RaycastHit2D hit = Physics2D.Linecast(transform.position, new Vector2(xpos, ypos),1<<LayerMask.NameToLayer("Action"));
+        RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, mDir, 5000f, 1<<LayerMask.NameToLayer("Action"));
         //this one is working great
-        if(hit.collider != null)
+        //if(hit.collider != null)
+        if(hit)
         {
+            Debug.Log("Hit somthing: " + hit.collider.name);
+            hit.transform.GetComponent<SpriteRenderer>().color = Color.red;
             
             Vector2 direction = new Vector2(0, 0);
-            direction.x = hit.point.x - transform.position.x  ;
-            direction.y = hit.point.y - transform.position.y;
+            direction.x = hit.point.x - gameObject.transform.position.x;
+            direction.y = hit.point.y - gameObject.transform.position.y;
 
             gameObject.GetComponent<Rigidbody2D>().AddForce((direction * 5), ForceMode2D.Impulse);
         }
