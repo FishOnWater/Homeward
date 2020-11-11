7e6de698de75f23831923a36b2d4567fa0917e79
Fish On Water <luismiguelfigueiras@gmail.com>
2020-11-11 08:57:23 +0000

    Cleanup commit
    Start working on camera transition in between screens. Not concluded yet.
    Mainly just a cleanup for the branch merge with branch level.

---
+++ Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    Vector2 nextPos;
+    Vector2 currentPosition;
+    int screen;
+    float camSpeed;
+
+    public Camera cam;
+    public Transform screen1;
+    public Transform screen2;
+    public Transform screen3;
+
+    void Start(){
+        screen = 1;
+        cam.position = screen1.position;
+    }
+    void OnTriggerEnter2D(Collider2D col, int check){
+        if(col.gameObject.tag == "Transition1"){
+            if(currentPosition == screen1.position){
+                nextPos = screen2.position;
+                currentPosition = Vector2.MoveTowards(currentPosition, nextPos, camSpeed * time.deltaTime);
+                screen = 2;
+            }
+            
+        }
+    }
+}
