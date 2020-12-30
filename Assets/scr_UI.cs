b8502d1b76a5398371fc28bf1f027130e37c6408
Thewildcard-pt <thewildcardpt@gmail.com>
2020-12-30 16:56:31 +0000

    In game ui ready to be implented
    displays:
    -time
    -ropes
    -deathcount(needs to be tied to the actual game)

---
+++ Assets/scr_UI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_UI : MonoBehaviour
+{
+
+
+    public PlayerController player;
+    public Text rope,deathtext,timer;
+    public int DeathCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        DeathCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rope.text = "Ropes : " + player.hooks;
+        timer.text = Time.time.ToString("0.00");
+        deathtext.text = "DeathCount: " + DeathCount;
+        if(Input.GetKeyDown(KeyCode.R))DeathCount++;
+    }
+
+    public void WriteRopes()
+    {
+        
+    }
+}
