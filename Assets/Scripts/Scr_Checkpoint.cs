7d98ede522207bcc32e9132ebf23db4b9e04bde8
Thewildcard-pt <thewildcardpt@gmail.com>
2021-01-11 08:14:37 +0000

    Checkpoint working
    pus um script para atualizar o checkpoint currente com colisoes

---
+++ Assets/Scripts/Scr_Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Checkpoint : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("checkpoint updated");
+            other.gameObject.GetComponent<PlayerController>().checkpoint = this.gameObject;            
+        }
+    }
+ }
