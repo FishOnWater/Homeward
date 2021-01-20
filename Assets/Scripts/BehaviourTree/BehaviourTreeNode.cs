71bb927d31dc3a88d9648895b629682749c11a11
Pedro Lopes <pedrocamper42v3@gmail.com>
2021-01-20 23:01:20 +0000

    a

---
+++ Assets/Scripts/BehaviourTree/BehaviourTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTreeNode 
+{
+    public enum Result {Running, Failure, Success};
+
+    public BehaviourTree Tree { get; set; }
+
+    public BehaviourTreeNode(BehaviourTree t)
+    {
+        Tree = t;
+    }
+
+    public virtual Result Execute()
+    {
+        return Result.Failure;
+    }
+}
