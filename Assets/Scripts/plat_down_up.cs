b930d72c784e6cf926c36f0fa4f07b10b1db26ae
Fish On Water <luismiguelfigueiras@gmail.com>
2021-01-23 20:22:04 +0000

    Some changes

--- Assets/Scripts/plat_down_up.cs
+++ Assets/Scripts/plat_down_up.cs
@@ -31,4 +31,14 @@ public class plat_down_up : MonoBehaviour
     private void OnDrawGizmos(){
         Gizmos.DrawLine(pos1.position, pos2.position);
     }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player")){
+            other.collider.transform.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other){
+        other.collider.transform.SetParent(null);
+    }
 }
